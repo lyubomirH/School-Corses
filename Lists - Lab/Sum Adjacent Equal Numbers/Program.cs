6370a6d41ff72836199dd5bb8b85f63ead60f7b3
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2023-11-14 17:45:21 +0200

    new Lab - lists
    it is not full

---
+++ Lists - Lab/Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Sum_Adjacent_Equal_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> nums = Console.ReadLine()
+                .Split(" ")
+                .Select(double.Parse)
+                .ToList();
+
+            Console.WriteLine( Sum(nums));
+        }
+        static string Sum(List<double> nums)
+        {
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (i == nums.Count - 1)
+                {
+                    break;
+                }
+                if (nums[i] == nums[i + 1])
+                {
+                    nums[i] += nums[i + 1];
+                    nums.RemoveAt(i + 1);
+                    i += -1;
+                }
+
+            }
+            return string.Join(" ", nums);
+        }
+    }
+}
