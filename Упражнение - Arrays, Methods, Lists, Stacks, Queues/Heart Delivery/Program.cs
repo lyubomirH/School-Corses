b862038254020e965d81116797cafe3366d5b271
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2023-12-19 11:17:10 +0200

    new home work

---
+++ Упражнение - Arrays, Methods, Lists, Stacks, Queues/Heart Delivery/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Heart_Delivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> houses = Console.ReadLine().Split("@").Select(int.Parse).ToList();
+            string[] command = Console.ReadLine().Split().ToArray();
+            int lastPosition = 0;
+            while (command[0] != "Love!")
+            {
+                int length = int.Parse(command[1]);
+                int currentIndex = lastPosition + length;
+                if (currentIndex >= houses.Count)
+                {
+                    currentIndex = 0;
+                }
+                if (houses[currentIndex] > 0)
+                {
+                    houses[currentIndex] -= 2;
+                    if (houses[currentIndex] == 0)
+                    {
+                        Console.WriteLine($"Place {currentIndex} has Valentine's day.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Place {currentIndex} already had Valentine's day.");
+                }
+                lastPosition = currentIndex;
+                command = Console.ReadLine().Split().ToArray();
+            }
+            Console.WriteLine($"Cupid's last position was {lastPosition}.");
+
+            if (houses.Sum() == 0)
+            {
+                Console.WriteLine("Mission was successful.");
+            }
+            else
+            {
+                int count = houses.Count(points => points > 0);
+                Console.WriteLine($"Cupid has failed {count} places.");
+            }
+        }
+    }
+}
