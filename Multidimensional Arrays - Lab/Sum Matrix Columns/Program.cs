6878110bccf6d0afccd8d38983812111398603fe
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2024-01-09 16:23:24 +0200

    new - Multidimensional and Jagged Array

---
+++ Multidimensional Arrays - Lab/Sum Matrix Columns/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[size[0], size[1]];
+            for (int r = 0; r <= size[0] - 1; r++)
+            {
+                int[] Elements = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    matrix[r, c] = Elements[c];
+                }
+            }
+            for(int c = 0; c < matrix.GetLength(1); c++)
+            {
+                int sum = 0;
+                for(int r = 0; r <matrix.GetLength(0); r++)
+                {
+                    sum += matrix[r, c];
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
---
+++ Multidimensional Arrays - Lab/Sum Matrix Elements/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray(); 
+                int[,] matrix = new int[size[0], size[1]];
+            for (int r = 0; r <= size[0] -1; r++)
+            {
+                int[] Elements = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+                for(int h = 0; h < matrix.GetLength(1); h++)
+                {
+                    matrix[r, h] = Elements[h];
+                }
+            }
+            int sum = 0;
+            for(int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for(int h = 0; h < matrix.GetLength(1); h++)
+                {
+                    sum += matrix[r, h];
+                }
+            }
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+
+        }
+    }
+}
