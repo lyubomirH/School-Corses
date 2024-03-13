e3da7e2dbbedc4fa0b4bfca8c506e6bfe3260978
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2024-03-13 14:11:46 +0200

    new - Lab: Strings and Text Processing

---
+++ Lab - Strings and Text Processing/Text Filter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            StringBuilder text = new StringBuilder(Console.ReadLine());
+            foreach (string word in bannedWords)
+            {
+                StringBuilder NewWord = new StringBuilder();
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    NewWord.Append("*");
+                }
+                string newWord = NewWord.ToString();
+               text.Replace(word, newWord);
+            }
+            string newText = text.ToString();
+            Console.WriteLine(newText);
+
+        }
+    }
+}
