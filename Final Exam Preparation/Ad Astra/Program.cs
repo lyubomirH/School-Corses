d68b668e50522b7d069d8f53278d488ccd2f51c6
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2024-06-09 10:09:28 +0300

    new

---
+++ Final Exam Preparation/Ad Astra/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Ad_Astra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([|#])(?<item>[A-z\s]+)\1(?<date>\d{2}\/\d{2}\/\d{2})\1(?<calories>[0-9]{1,4}|10000)\1";
+            string text = Console.ReadLine();
+            int caloroes = 0;
+
+            foreach(Match match in Regex.Matches(text, pattern))
+            {
+                caloroes += int.Parse(match.Groups["calories"].Value);
+            }
+            int totalDays = caloroes / 2000;
+            Console.WriteLine($"You have food to last you for: {totalDays} days!");
+
+            foreach (Match match in Regex.Matches(text, pattern))
+            {
+                Console.WriteLine($"Item: {match.Groups["item"].Value}, Best before: {match.Groups["date"].Value}, Nutrition: {match.Groups["calories"].Value}");
+            }
+        }
+    }
+}
---
+++ Final Exam Preparation/The Enigma Machine/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace The_Enigma_Machine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            string input;
+            while ((input = Console.ReadLine()) != "Decode")
+            {
+                string[] splitted = input.Split("|");
+                string command = splitted[0];
+
+                switch (command)
+                {
+                    case "Move":
+                        int lettersNumbers = int.Parse(splitted[1]);
+                        string textToMove = message.Substring(0, lettersNumbers);
+
+                        message = message.Remove(0,lettersNumbers);
+                        message += textToMove;
+                    break;
+                    case "Insert":
+                        int index = int.Parse(splitted[1]);
+                        string value = splitted[2];
+
+                        message = message.Insert(index, value);
+                    break;
+                    case "ChangeAll":
+                        string substring = splitted[1];
+                        string replacement = splitted[2];
+
+                        message = message.Replace(substring, replacement);
+                    break;
+                }
+            }
+            Console.WriteLine($"The decrypted message is: {message}");
+        }
+    }
+}
